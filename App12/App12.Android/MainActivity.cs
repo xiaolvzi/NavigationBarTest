e8deddc96e5bbce11bb231d30407043d85d44459
Joe Lv <joel@wicresoft.com>
2018-04-13 17:03:18 +0800

    first commit

---
+++ App12/App12.Android/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App12.Droid
+{
+    [Activity(Label = "App12", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity,View.IOnSystemUiVisibilityChangeListener
+    {
+
+        int uiOptions;
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+            this.Window.AddFlags(WindowManagerFlags.Fullscreen); // hide the status bar
+
+            uiOptions = (int)Window.DecorView.SystemUiVisibility;
+
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.HideNavigation;
+            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+
+            Window.DecorView.SetOnSystemUiVisibilityChangeListener(this);
+        }
+
+        public void OnSystemUiVisibilityChange([GeneratedEnum] StatusBarVisibility visibility)
+        {
+            if (((int)visibility & (int)SystemUiFlags.Fullscreen) == 0)
+            {
+                Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+            }
+            
+        }
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            base.OnWindowFocusChanged(hasFocus);
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)((int)SystemUiFlags.Fullscreen
+                | (int)SystemUiFlags.ImmersiveSticky
+                | (int)SystemUiFlags.LayoutHideNavigation
+                | (int)SystemUiFlags.LayoutStable
+                | (int)SystemUiFlags.HideNavigation);
+        }
+    }
+}
+
---
+++ App12/App12.Android/obj/Debug/android/src/md52d1aeec55c0afe9e67c0954c6fa15888/MainActivity.java
@@ -0,0 +1,66 @@
+package md52d1aeec55c0afe9e67c0954c6fa15888;
+
+
+public class MainActivity
+	extends md5b60ffeb829f638581ab2bb9b1a7f4f3f.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		android.view.View.OnSystemUiVisibilityChangeListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onWindowFocusChanged:(Z)V:GetOnWindowFocusChanged_ZHandler\n" +
+			"n_onSystemUiVisibilityChange:(I)V:GetOnSystemUiVisibilityChange_IHandler:Android.Views.View/IOnSystemUiVisibilityChangeListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("App12.Droid.MainActivity, App12.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("App12.Droid.MainActivity, App12.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onWindowFocusChanged (boolean p0)
+	{
+		n_onWindowFocusChanged (p0);
+	}
+
+	private native void n_onWindowFocusChanged (boolean p0);
+
+
+	public void onSystemUiVisibilityChange (int p0)
+	{
+		n_onSystemUiVisibilityChange (p0);
+	}
+
+	private native void n_onSystemUiVisibilityChange (int p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
